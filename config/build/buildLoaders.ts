185426236596a033652c7835ec58e2ec36dc996d
Marat Musin <maratmusin@MBP-Marat.Dlink>
2023-09-06 23:29:48 +0300

    storybook webpack config added

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
+import { buildCssLoaders } from "./loaders/buildCssLoaders";
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   const svgLoader = {
@@ -8,27 +8,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     use: ["@svgr/webpack", "url-loader"],
   };
 
-  const cssLoader = {
-    test: /\.s[ac]ss$/i,
-    use: [
-      // Creates `style` nodes from JS strings
-      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-      // Translates CSS into CommonJS
-      {
-        loader: "css-loader",
-        options: {
-          modules: {
-            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
-            localIdentName: isDev
-              ? "[local]--[hash:base64:5]"
-              : "[hash:base64:8]",
-          },
-        },
-      },
-      // Compiles Sass to CSS
-      "sass-loader",
-    ],
-  };
+  const cssLoader = buildCssLoaders(isDev);
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: "ts-loader",
