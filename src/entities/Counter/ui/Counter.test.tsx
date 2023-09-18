3c4aaf07531b4de7d34c46c28525112c73b80dcb
Marat Musin <maratmusin@MacBook-Pro-Marat.local>
2023-09-18 21:42:02 +0300

    testin

--- src/entities/Counter/ui/Counter.test.tsx
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -10,18 +10,5 @@ describe("Counter", () => {
     });
     expect(screen.getByTestId("value-title")).toHaveTextContent("10");
   });
-  // test("increment", () => {
-  //   componentRender(<Counter />, {
-  //     initialState: { counter: { value: 10 } },
-  //   });
-  //   userEvent.click(screen.getByTestId("increment-btn"));
-  //   expect(screen.getByTestId("value-title")).toHaveTextContent("11");
-  // });
-  // test("decrement", () => {
-  //   componentRender(<Counter />, {
-  //     initialState: { counter: { value: 10 } },
-  //   });
-  //   userEvent.click(screen.getByTestId("decrement-btn"));
-  //   expect(screen.getByTestId("value-title")).toHaveTextContent("9");
-  // });
+
 });
